03bd8443e5e099eb872930a90b21dfb1d1d8930c
Paul Kerr <pkholdingsuk@gmail.com>
2019-12-07 19:32:23 +0000

    Updates

---
+++ precache-manifest.665b2deb3c37c567b53c4be8a9fded2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a62b53cfa970d49e1d9555eec8792ed",
+    "url": "/monster-directory/index.html"
+  },
+  {
+    "revision": "7019d4d924f59751217c",
+    "url": "/monster-directory/static/css/main.2187c432.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monster-directory/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "7019d4d924f59751217c",
+    "url": "/monster-directory/static/js/main.6b504562.chunk.js"
+  },
+  {
+    "revision": "8cead65944e45d317bdd",
+    "url": "/monster-directory/static/js/runtime-main.fb21dd39.js"
+  }
+]);
